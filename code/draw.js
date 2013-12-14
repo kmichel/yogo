4e6b6885842054aa67b04af61fe96b56819af85c
Kevin Michel <kha.hem@gmail.com>
2013-12-14 14:47:00 +0100

    Added background grid

--- code/draw.js
+++ code/draw.js
@@ -1,13 +1,42 @@
 function draw_game(game, context) {
     draw_background(game, context);
-    draw_player(game.player, context);
+    draw_grid(game, context);
     for (var i = 0; i < game.bots.length; ++i)
         draw_bot(game.bots[i], context);
+    draw_player(game.player, context);
 }
 
 function draw_background(game, context) {
     context.fillStyle = game.background_color;
     context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+}
+
+function draw_grid(game, context) {
+    context.lineWidth = 1;
+    draw_subgrid(game.grid.minor_color, game.grid.minor_spacing, context);
+    draw_subgrid(game.grid.major_color, game.grid.major_spacing, context);
+}
+
+function draw_subgrid(color, spacing, context) {
+    var width = context.canvas.width;
+    var height = context.canvas.height;
+    context.strokeStyle = color;
+    context.beginPath();
+    for (var x = 0; x <= width; x += spacing) {
+        context.moveTo(x + .5, 0);
+        context.lineTo(x + .5, height);
+    }
+    for (var y = 0; y <= height; y += spacing) {
+        context.moveTo(0, y + .5);
+        context.lineTo(width, y + .5);
+    }
+    for (var i = 0 - height; i <= width + height; i += spacing * 2) {
+        context.moveTo(i + 1.5, .5);
+        context.lineTo(i + 1.5 - height, height + .5);
+        context.moveTo(i + 1.5, .5);
+        context.lineTo(i + 1.5 + height, height + .5);
+    }
+    context.stroke();
 }
 
 function draw_player(player, context) {
--- data.js
+++ data.js
@@ -1,6 +1,12 @@
 var yogo = {
     tick_fps: 60,
-    background_color: '#000',
+    background_color: '#2e3436',
+    grid: {
+        minor_color: 'rgba(255, 255, 255, 0.05)',
+        minor_spacing: 20,
+        major_color: 'rgba(255, 255, 255, 0.10)',
+        major_spacing: 100
+    },
     key_map: {
         32: 'space',
         37: 'left',
@@ -19,20 +25,20 @@ var yogo = {
         position: {x: 50, y: 50},
         direction: {x: 0, y: 0},
         speed: 2,
-        radius: 10,
-        direction_radius: 20,
-        color: '#fff'
+        radius: 5,
+        direction_radius: 10,
+        color: '#9cd1e3'
     },
     bots: [
         {
             position: {x: 200, y: 100},
             radius: 5,
-            color: '#a40000'
+            color: '#a45300'
         },
         {
             position: {x: 100, y: 200},
             radius: 5,
-            color: '#a40000'
+            color: '#a45300'
         }
     ]
 };
