358aa5ab9c2f3c85f62e063c99b439a1eac96173
Kevin Michel <kha.hem@gmail.com>
2013-12-15 22:47:01 +0100

    Nothing

--- levels/level_end.js
+++ levels/level_end.js
@@ -3,6 +3,66 @@ yogo.levels.push({
         {
             text: "Congratulation, you're done :)",
             rect: {x: 0, y: 0, width: 601, height: 401}
+        },
+        {
+            text: "B",
+            rect: {x: 0, y: 0, width: 40, height: 40}
+        },
+        {
+            text: "I",
+            rect: {x: 40, y: 0, width: 40, height: 40}
+        },
+        {
+            text: "E",
+            rect: {x: 80, y: 0, width: 40, height: 40}
+        },
+        {
+            text: "B",
+            rect: {x: 120, y: 0, width: 40, height: 40}
+        },
+        {
+            text: "E",
+            rect: {x: 160, y: 0, width: 40, height: 40}
+        },
+        {
+            text: "R",
+            rect: {x: 200, y: 0, width: 40, height: 40}
+        },
+        {
+            text: " ",
+            rect: {x: 240, y: 0, width: 40, height: 40}
+        },
+        {
+            text: "4",
+            rect: {x: 280, y: 0, width: 40, height: 40}
+        },
+        {
+            text: "E",
+            rect: {x: 320, y: 0, width: 40, height: 40}
+        },
+        {
+            text: "V",
+            rect: {x: 360, y: 0, width: 40, height: 40}
+        },
+        {
+            text: "A",
+            rect: {x: 400, y: 0, width: 40, height: 40}
+        },
+        {
+            text: "R",
+            rect: {x: 440, y: 0, width: 40, height: 40}
+        },
+        {
+            text: " ",
+            rect: {x: 480, y: 0, width: 40, height: 40}
+        },
+        {
+            text: "<",
+            rect: {x: 520, y: 0, width: 40, height: 40}
+        },
+        {
+            text: "3",
+            rect: {x: 560, y: 0, width: 41, height: 40}
         }
     ],
     player: {
