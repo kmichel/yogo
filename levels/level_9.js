4817abf143e3b49ba864cce83225466e6ff4b251
Kevin Michel <kha.hem@gmail.com>
2013-12-15 20:43:32 +0100

    Add level 9

---
+++ levels/level_9.js
@@ -0,0 +1,68 @@
+yogo.levels.push({
+    hints: [
+    ],
+    player: {
+        position: {x: 50.5, y: 370.5}
+    },
+    exit: {
+        position: {x: 300.5, y: 200.5}
+    },
+    bots: [
+        {
+            position: {x: 300.5, y: 80.5}
+        },
+        {
+            position: {x: 300.5, y: 320.5}
+        }
+    ],
+    barriers: {
+        emitters: [
+            {
+                position: {x: 280.6, y: 180.5},
+                direction: {x: 1, y: 0}
+            }
+        ],
+        reflectors: [
+            {
+                position: {x: 320.5, y: 180.5},
+                reflection: 'counter-clockwise'
+            },
+            {
+                position: {x: 320.5, y: 220.5},
+                reflection: 'counter-clockwise'
+            },
+            {
+                position: {x: 200.5, y: 220.5},
+                reflection: 'counter-clockwise'
+            },
+            {
+                position: {x: 200.5, y: 120.5},
+                reflection: 'counter-clockwise'
+            },
+            {
+                position: {x: 400.5, y: 120.5},
+                reflection: 'counter-clockwise'
+            },
+            {
+                position: {x: 400.5, y: 280.5},
+                reflection: 'counter-clockwise'
+            },
+            {
+                position: {x: 100.5, y: 280.5},
+                reflection: 'counter-clockwise'
+            },
+            {
+                position: {x: 100.5, y: 60.5},
+                reflection: 'counter-clockwise'
+            },
+            {
+                position: {x: 500.5, y: 60.5},
+                reflection: 'counter-clockwise'
+            },
+            {
+                position: {x: 500.5, y: 340.5},
+                reflection: 'counter-clockwise'
+            }
+        ]
+    }
+});
