698a2f99648228fd709422e42bd5411e919c3d92
Kevin Michel <kha.hem@gmail.com>
2013-12-15 18:53:13 +0100

    Add level 6

---
+++ levels/level_6.js
@@ -0,0 +1,30 @@
+yogo.levels.push({
+    hints: [
+    ],
+    player: {
+        position: {x: 40.5, y: 200.5}
+    },
+    exit: {
+        position: {x: 500.5, y: 200.5}
+    },
+    bots: [
+        {
+            position: {x: 280.5, y: 100.5}
+        },
+        {
+            position: {x: 380.5, y: 200.5}
+        },
+        {
+            position: {x: 280.5, y: 300.5}
+        }
+    ],
+    barriers: {
+        emitters: [
+            {
+                position: {x: 200.5, y: 0.5},
+                direction: {x: 0, y: 1}
+            }
+        ],
+        reflectors: []
+    }
+});
