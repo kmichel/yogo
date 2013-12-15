6857d0a02974f9b861b9b5ac6c05963d355d664e
Kevin Michel <kha.hem@gmail.com>
2013-12-15 21:48:22 +0100

    Add level 10

---
+++ levels/level_10.js
@@ -0,0 +1,122 @@
+yogo.levels.push({
+    hints: [
+    ],
+    player: {
+        position: {x: 20.5, y: 20.5}
+    },
+    exit: {
+        position: {x: 580.5, y: 380.5}
+    },
+    bots: [
+        {
+            position: {x: 200.5, y: 0.5}
+        },
+        {
+            position: {x: 100.5, y: 100.5}
+        },
+        {
+            position: {x: 100.5, y: 300.5}
+        },
+        {
+            position: {x: 200.5, y: 400.5}
+        },
+        {
+            position: {x: 400.5, y: 400.5}
+        },
+        {
+            position: {x: 500.5, y: 300.5}
+        },
+        {
+            position: {x: 500.5, y: 100.5}
+        },
+        {
+            position: {x: 400.5, y: 0.5}
+        }
+    ],
+    barriers: {
+        emitters: [
+            {
+                position: {x: 0.5, y: 80.5},
+                direction: {x: 1, y: 1}
+            },
+            {
+                position: {x: 0.5, y: 360.5},
+                direction: {x: 1, y: 0}
+            },
+            {
+                position: {x: 280.5, y: 400.5},
+                direction: {x: 0, y: -1}
+            },
+            {
+                position: {x: 320.5, y: 400.5},
+                direction: {x: -1, y: -1}
+            },
+            {
+                position: {x: 120.5, y: 400.5},
+                direction: {x: 0, y: -1}
+            },
+            {
+                position: {x: 600.5, y: 140.5},
+                direction: {x: -1, y: 0}
+            },
+            {
+                position: {x: 600.5, y: 280.5},
+                direction: {x: -1, y: -1}
+            },
+            {
+                position: {x: 600.5, y: 120.5},
+                direction: {x: -1, y: 1}
+            },
+            {
+                position: {x: 520.5, y: 0.5},
+                direction: {x: 0, y: 1}
+            }
+        ],
+        reflectors: [
+            {
+                position: {x: 20.5, y: 100.5},
+                reflection: 'clockwise'
+            },
+            {
+                position: {x: 240.5, y: 240.5},
+                reflection: 'clockwise'
+            },
+            {
+                position: {x: 160.5, y: 240.5},
+                reflection: 'counter-clockwise'
+            },
+            {
+                position: {x: 240.5, y: 360.5},
+                reflection: 'clockwise'
+            },
+            {
+                position: {x: 200.5, y: 280.5},
+                reflection: 'clockwise'
+            },
+            {
+                position: {x: 120.5, y: 200.5},
+                reflection: 'counter-clockwise'
+            },
+            {
+                position: {x: 200.5, y: 140.5},
+                reflection: 'counter-clockwise'
+            },
+            {
+                position: {x: 420.5, y: 100.5},
+                reflection: 'clockwise'
+            },
+            {
+                position: {x: 460.5, y: 260.5},
+                reflection: 'clockwise'
+            },
+            {
+                position: {x: 520.5, y: 360.5},
+                reflection: 'counter-clockwise'
+            },
+            {
+                position: {x: 360.5, y: 360.5},
+                reflection: 'counter-clockwise'
+            }
+        ]
+    }
+});
