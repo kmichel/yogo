628fd0d10c73e07361a1eabf75741f557e579e05
Kevin Michel <kha.hem@gmail.com>
2013-12-16 02:41:07 +0100

    Add guitar level

---
+++ levels/guitar.js
@@ -0,0 +1,38 @@
+yogo.levels.push({
+    hints: [
+    ],
+    player: {
+        position: {x: 100.5, y: 200.5}
+    },
+    exit: {
+        position: {x: 280.5, y: 200.5}
+    },
+    bots: [
+        {
+            position: {x: 180.5, y: 100.5}
+        },
+        {
+            position: {x: 320.5, y: 200.5}
+        },
+        {
+            position: {x: 460.5, y: 300.5}
+        }
+    ],
+    barriers: {
+        emitters: [
+            {
+                position: {x: 40.5, y: 0.5},
+                direction: {x: 1, y: 1}
+            },
+            {
+                position: {x: 80.5, y: 0.5},
+                direction: {x: 1, y: 1}
+            },
+            {
+                position: {x: 120.5, y: 0.5},
+                direction: {x: 1, y: 1}
+            }
+        ],
+        reflectors: []
+    }
+});
